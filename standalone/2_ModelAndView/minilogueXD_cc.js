fe1aa6e0f24c74b4428d376c2049e72935504736
Matthew MacLeod <mgmacleod@posteo.org>
2021-04-04 10:14:23 -0400

    GH-3: updates the CC model to match the OG implementation; removes unneeded functions

--- standalone/2_ModelAndView/minilogueXD_cc.js
+++ standalone/2_ModelAndView/minilogueXD_cc.js
@@ -3,129 +3,47 @@ var randomInt = utils.randomInt;
 var lsbCC = utils.lsbCC;
 
 var cctls = [
-	{ name: "PORTAMENTO", cc: 5, min: 0, max: 127 },
-	{ name: "AMP EG ATTACK", cc: 16, min: 0, max: 1023 },
-	{ name: "AMP EG DECAY", cc: 17, min: 0, max: 1023 },
-	{ name: "AMP EG SUSTAIN", cc: 18, min: 0, max: 1023 },
-	{ name: "AMP EG RELEASE", cc: 19, min: 0, max: 1023 },
-	{ name: "EG ATTACK", cc: 20, min: 0, max: 1023 },
-	{ name: "EG DECAY", cc: 21, min: 0, max: 1023 },
-	{ name: "EG INT", cc: 22, min: 0, max: 1023 },
-	{ name: "EG TARGET", cc: 23, min: 0, max: 127, enum: [ "CUTOFF", "PITCH2", "PITCH" ] },
-	{ name: "LFO RATE", cc: 24, min: 0, max: 1023 },
-	{ name: "LFO INT", cc: 26, min: 0, max: 1023 },
-	// { name: "VOICE MODE DEPTH", cc: 27, min: 0, max: 1023 },
-	{ name: "MOD FX TIME", cc: 28, min: 0, max: 1023 },
-	{ name: "MOD FX DEPTH", cc: 29, min: 0, max: 1023 },
-	{ name: "MULTI LEVEL", cc: 33, min: 0, max: 1023 },
-	{ name: "VCO 1 PITCH", cc: 34, min: 0, max: 1023 },
-	{ name: "VCO 2 PITCH", cc: 35, min: 0, max: 1023 },
-	{ name: "VCO 1 SHAPE", cc: 36, min: 0, max: 1023 },
-	{ name: "VCO 2 SHAPE", cc: 37, min: 0, max: 1023 },
-	{ name: "VCO 1 LEVEL", cc: 39, min: 0, max: 1023 },
-	{ name: "VCO 2 LEVEL", cc: 40, min: 0, max: 1023 },
-	{ name: "CROSS MOD DEPTH", cc: 41, min: 0, max: 1023 },
-	{ name: "CUTOFF", cc: 43, min: 0, max: 1023 },
-	{ name: "RESONANCE", cc: 44, min: 0, max: 1023 },
+	//{ name: "PORTAMENTO", cc: 5, min: 0, max: 127 },
+	{ name: "AMP EG ATTACK", cc: 16, min: 0, max: 127 },
+	{ name: "AMP EG DECAY", cc: 17, min: 0, max: 127 },
+	{ name: "AMP EG SUSTAIN", cc: 18, min: 0, max: 127 },
+	{ name: "AMP EG RELEASE", cc: 19, min: 0, max: 127 },
+	{ name: "EG ATTACK", cc: 20, min: 0, max: 127 },
+	{ name: "EG DECAY", cc: 21, min: 0, max: 127 },
+	{ name: "EG SUSTAIN", cc: 22, min: 0, max: 127 },
+	{ name: "EG RELEASE", cc: 23, min: 0, max: 127 },
+	{ name: "LFO RATE", cc: 24, min: 0, max: 127 },
+	{ name: "LFO INT", cc: 26, min: 0, max: 127 },
+	{ name: "VOICE MODE DEPTH", cc: 27, min: 0, max: 127 },
+	{ name: "DELAY HI PASS CUTOFF", cc: 29, min: 0, max: 127 },
+	{ name: "DELAY TIME", cc: 30, min: 0, max: 127 },
+	{ name: "DELAY FEEDBACK", cc: 31, min: 0, max: 127 },
+	{ name: "NOISE LEVEL", cc: 33, min: 0, max: 127 },
+	{ name: "VCO 1 PITCH", cc: 34, min: 0, max: 127 },
+	{ name: "VCO 2 PITCH", cc: 35, min: 0, max: 127 },
+	{ name: "VCO 1 SHAPE", cc: 36, min: 0, max: 127 },
+	{ name: "VCO 2 SHAPE", cc: 37, min: 0, max: 127 },
+	{ name: "VCO 1 LEVEL", cc: 39, min: 0, max: 127 },
+	{ name: "VCO 2 LEVEL", cc: 40, min: 0, max: 127 },
+	{ name: "VCO 2 CROSS MOD DEPTH", cc: 41, min: 0, max: 127 },
+	{ name: "VCO 2 PITCH EG INT", cc: 42, min: 0, max: 127 },
+	{ name: "CUTOFF", cc: 43, min: 0, max: 127 },
+	{ name: "RESONANCE", cc: 44, min: 0, max: 127 },
+	{ name: "CUTOFF EG INT", cc: 45, min: 0, max: 127 },
 	{ name: "VCO 1 OCTAVE", cc: 48, min: 0, max: 127, enum: [ "16'", "8'", "4'", "2'" ] },
 	{ name: "VCO 2 OCTAVE", cc: 49, min: 0, max: 127, enum: [ "16'", "8'", "4'", "2'" ] },
 	{ name: "VCO 1 WAVE", cc: 50, min: 0, max: 127, enum: [ "SQR", "TRI", "SAW" ] },
 	{ name: "VCO 2 WAVE", cc: 51, min: 0, max: 127, enum: [ "SQR", "TRI", "SAW" ] },
-	{ name: "MULTI TYPE", cc: 53, min: 0, max: 127, randomMax: 85, enum: [ "NOISE", "VPM", "USER" ] }, // leave the user bank out of this
-	{ name: "MULTI SHAPE", cc: 54, min: 0, max: 1023 }, // interface, but not data
 	{ name: "LFO TARGET", cc: 56, min: 0, max: 127, enum: [ "CUTOFF", "SHAPE", "PITCH" ] },
-	{ name: "LFO WAVE", cc: 57, min: 0, max: 127, enum: [ "SQR", "TRI", "SAW" ] },
-	{ name: "LFO MODE", cc: 58, min: 0, max: 127, enum: [ "1-SHOT", "NORMAL", "BPM" ] },
-	{ name: "VOICE MODE DEPTH", cc: 59, min: 0, max: 1023 },
-	// { name: "DAMPER", cc: 64, min: 0, max: 127, randomize: false }, // damper pedal
+	{ name: "LFO EG", cc: 57, min: 0, max: 127, enum: [ "OFF", "RATE", "INT" ] },
+	{ name: "LFO WAVE", cc: 58, min: 0, max: 127, enum: [ "SQR", "TRI", "SAW" ] },
 	{ name: "SYNC", cc: 80, min: 0, max: 127, enum: [ "OFF", "ON" ] },
 	{ name: "RING", cc: 81, min: 0, max: 127, enum: [ "OFF", "ON" ] },
+	{ name: "CUTOFF VELOCITY", cc: 82, min: 0, max: 127, enum: [ "0%", "50%", "100%" ] },
 	{ name: "CUTOFF KEYTRACK", cc: 83, min: 0, max: 127, enum: [ "0%", "50%", "100%" ] },
-	{ name: "CUTOFF DRIVE", cc: 84, min: 0, max: 127, enum: [ "0%", "50%", "100%" ]  },
-	{ name: "MOD FX TYPE", cc: 88, min: 0, max: 127, randomMax: 101, enum: [
-			"CHORUS", "ENSEMBLE", "PHASER", "FLANGER", "USER"
-		]
-	},
-	{ name: "DELAY SUB TYPE", cc: 89, min: 0, max: 127, randomMax: 76, enum: [
-			"STEREO", "MONO", "PING PONG", "HIPASS",
-			"TAPE", "ONE TAP", "STEREO BPM", "MONO BPM",
-			"PING BPM", "HIPASS BPM", "TAPE BPM", "DOUBLING",
-			"USER1", "USER2", "USER3", "USER4", "USER5", "USER6", "USER7", "USER8"
-		]
-	},
-	{ name: "REVERB SUB TYPE", cc: 90, min: 0, max: 127, randomMax: 71, enum: [
-			"HALL", "SMOOTH", "ARENA", "PLATE",
-			"ROOM", "EARLY REF", "SPACE", "RISER",
-			"SUBMARINE", "HORROR", "USER1", "USER2",
-			"USER3", "USER4", "USER5", "USER6", "USER7", "USER8"
-		]
-	},
-	{ name: "MOD FX ON-OFF", cc: 92, min: 0, max: 127, enum: [ "OFF", "ON" ] },
-	{ name: "DELAY ON-OFF", cc: 93, min: 0, max: 127, enum: [ "OFF", "ON" ] },
-	{ name: "REVERB ON-OFF", cc: 94, min: 0, max: 127, enum: [ "OFF", "ON" ] },
-	{ name: "MOD FX SUB TYPE", cc: 96, min: 0, max: 127, enumFn: getModFxSubType },
-	{ name: "MULTI SUB TYPE SELECT", cc: 103, min: 0, max: 127, enumFn: getMultiSubType },
-	{ name: "MULTI SHIFT SHAPE", cc: 104, min: 0, max: 1023 }, // interface, but not data
-	{ name: "DELAY TIME", cc: 105, min: 0, max: 1023 },
-	{ name: "DELAY DEPTH", cc: 106, min: 0, max: 1023 },
-	{ name: "DELAY DRY WET", cc: 107, min: 0, max: 1023 },
-	{ name: "REVERB TIME", cc: 108, min: 0, max: 1023 },
-	{ name: "REVERB DEPTH", cc: 109, min: 0, max: 1023 },
-	{ name: "REVERB DRY WET", cc: 110, min: 0, max: 1023 }
+	{ name: "CUTOFF TYPE", cc: 84, min: 0, max: 127, enum: [ "2 POLE", "4 POLE" ] },
+	{ name: "DELAY OUTPUT ROUTING", cc: 88, min: 0, max: 127, enum: [ "BYPASS", "POST HPF", "PRE HPF" ] }
 ];
-
-// the "MOD FX SUB TYPE" options depend on the value of "MOD FX TYPE"
-function getModFxSubType() {
-	var param = findName("MOD FX TYPE");
-	if (param) {
-		switch (param.getEnumString()) {
-			case "CHORUS":
-				return [ "STEREO", "LIGHT", "DEEP", "TRIPHASE", "HARMONIC", "MONO", "FEEDBACK", "VIBRATO" ];
-				break;
-			case "ENSEMBLE":
-				return [ "STEREO", "LIGHT", "MONO" ];
-				break;
-			case "PHASER":
-				return [ "STEREO", "FAST", "ORANGE", "SMALL", "SMALL RESO", "BLACK", "FORMANT", "TWINKLE"];
-				break;
-			case "FLANGER":
-				return [ "STEREO", "LIGHT", "MONO", "HIGH SWEEP", "MID SWEEP", "PAN SWEEP", "MONO SWEEP", "TRIPHASE" ];
-				break;
-			case "USER":
-				return [ "USER1", "USER2", "USER3", "USER4", "USER5", "USER6", "USER7", "USER8",
-							"USER9", "USER10", "USER11", "USER12", "USER13", "USER14", "USER15", "USER16" ];
-				break;
-			default:
-				return [];
-		}
-	}
-	else post("getModFxSubType: no param\n");
-	return [];
-}
-
-// the "MULTI SUB TYPE" options depend on the value of "MULTI TYPE"
-function getMultiSubType() {
-	var param = findName("MULTI TYPE");
-	if (param) {
-		switch (param.getEnumString()) {
-			case "NOISE":
-				return [ "HIGH", "LOW", "PEAK", "DECIM" ];
-				break;
-			case "VPM":
-				return [ "SIN1", "SIN2", "SIN3", "SIN4", "SAW1", "SAW2", "SQU1", "SQU2",
-							"FAT1", "FAT2", "AIR1", "AIR2", "DECAY1", "DECAY2", "CREEP", "THROAT" ];
-				break;
-			case "USER":
-				return [ "USER1", "USER2", "USER3", "USER4", "USER5", "USER6", "USER7", "USER8",
-							"USER9", "USER10", "USER11", "USER12", "USER13", "USER14", "USER15", "USER16" ];
-				break;
-			default:
-				return [];
-		}
-	}
-	else post("getMultiSubType: no param\n");
-	return [];
-}
 
 function findName(name) {
 	for (var c in cctls) {
